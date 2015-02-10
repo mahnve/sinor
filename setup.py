373a58bb9147b1a466f98734c41eed000f7cdcce
Marcus Ahnve <marcus@ahnve.com>
2015-02-10 15:12:24 +0100

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,39 @@
 __author__ = 'mahnve'
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-setup(description='HeadOn Blog',
+
+# Inspired by the example at https://pytest.org/latest/goodpractises.html
+class NoseTestCommand(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Run nose ensuring that argv simulates running nosetests directly
+        import nose
+        nose.run_exit(argv=['nosetests'])
+
+setup(description='Simple blog tool for static websites',
       author='Marcus Ahnve',
-      url='http://www.git',
+      url='http://github.com/mahnve/sinor',
       download_url='Where to download it.',
       author_email='m@hnve.org',
       version='0.1',
-      install_requires=[''],
+      install_requires=['Markdown',
+                        'pystache',
+                        'pyatom',
+                        'Pygments',
+                        'pyyaml',
+                        'setuptools',
+                        'lxml',
+                        'toml'],
       packages=[
           'sinor',
           'test'],
       scripts=['scripts/sinor'],
       name='sinor',
-      tests_require=['nosetests'],
-      cmdclass={'test': })
+      tests_require=['nose', 'mock'],
+      cmdclass={'test': NoseTestCommand})
