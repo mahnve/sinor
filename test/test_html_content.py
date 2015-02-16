10ed79739b191a1b4cb33166f3a0ee56ec3c9f35
Marcus Ahnve <marcus@ahnve.com>
2015-02-16 19:25:19 +0100

    Data builder for markdown and html

--- test/test_html_content.py
+++ test/test_html_content.py
@@ -1,38 +1,40 @@
 from sinor import html_content
 from nose.tools import assert_equals
-
+from data_builder import HTMLContentBuilder
 
 def test_parse_empty_string():
+    html = HTMLContentBuilder().build()
     assert_equals(html_content.from_string(''), {'title': '',
-                                           'date': '',
-                                           'content': ''})
+                                                 'date': '',
+                                                 'content': ''})
 
 
 def test_finds_a_post_title_class():
-    html = "<div><div id='post-title'>Title</div></div>"
+    html = HTMLContentBuilder().with_title('Title').build()
     assert_equals(html_content.from_string(html), {'title': 'Title',
-                                             'date': '',
-                                             'content': ''})
+                                                   'date': '',
+                                                   'content': ''})
 
 
 def test_finds_a_post_date():
-    html = "<div><time id='post-date'>2010-01-01</time></div>"
+    html = HTMLContentBuilder().with_post_date('2010-01-01').build()
     assert_equals(html_content.from_string(html), {'date': '2010-01-01',
                                              'title': '',
                                              'content': ''})
 
 
 def test_finds_post_content():
-    html = '<div id="post-content"><p>Ulysseus!</p></div>'
-    assert_equals(html_content.from_string(html), {'content': html,
-                                             'title': '',
-                                             'date': ''})
+    html = HTMLContentBuilder().with_content('<p>Ulysseus!</p>').build()
+    assert_equals(html_content.from_string(html),
+                  {'content': '<div id="post-content"><p>Ulysseus!</p></div>',
+                   'title': '',
+                   'date': ''})
 
 
 def test_multihtml():
-    html = '<div><div id="post-content"><h1 id="post-title">Foo</h1><time id="post-date">2010-01-01</time></div></div>'
+    html = HTMLContentBuilder().with_title('Foo').with_post_date('2010-01-01').with_content('<p>Hej</p>').build()
     assert_equals(html_content.from_string(html),
                   {'content':
-                   '<div id="post-content"><h1 id="post-title">Foo</h1><time id="post-date">2010-01-01</time></div>',
+                   '<div id="post-content"><p>Hej</p></div>',
                    'title': 'Foo',
                    'date': '2010-01-01'})
--- test/test_posts.py
+++ test/test_posts.py
@@ -2,22 +2,27 @@ from nose.tools import assert_equals
 from sinor import posts
 from sinor.config import config
 from mock import Mock
+from data_builder import MarkdownPostBuilder
 
 
 def test_draft():
-    a = {'a': 1}
-    b = {'draft': 1}
-    assert_equals(posts.no_drafts([a, b]), [a])
+    a = MarkdownPostBuilder().with_draft('true').build()
+    b = MarkdownPostBuilder().with_draft('True').build()
+    c = MarkdownPostBuilder().with_draft('false').build()
+    d = MarkdownPostBuilder().with_draft('').build()
+    assert_equals(posts.no_drafts([a, b, c, d]), [c, d])
 
 
 def test_limit_count():
-    a = [0, 1, 2, 3, 4, 5]
-    assert_equals([0, 1, 2], posts.limit(a, 3))
+    a = b = c = d = MarkdownPostBuilder().build()
+    plist = [a, b, c, d]
+    assert_equals([a, b], posts.limit(plist, 2))
 
 
 def test_limit_negative_count():
-    a = [0, 1, 2]
-    assert_equals(a, posts.limit(a, -1))
+    a = b = c = d = MarkdownPostBuilder().build()
+    post_list = [a, b, c, d]
+    assert_equals(post_list, posts.limit(post_list, -1))
 
 
 def test_default_partial_dir():
@@ -28,3 +33,5 @@ def test_default_partial_dir():
 def test_selects_config_partial_dir():
     config.build_partials_dir = Mock(return_value="/bar")
     assert_equals(posts.partials_dir("/foo/bar.mustache"), "/bar")
+
+
